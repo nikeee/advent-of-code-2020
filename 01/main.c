f02d339e0f731c87c0c0d923e18de550f0164bc2
Niklas Mollenhauer <nikeee@outlook.com>
2020-12-01 20:20:33 +0100

    Solve 1: 2/2

--- 01/main.c
+++ 01/main.c
@@ -29,7 +29,6 @@ int *read_input(const size_t input_size)
 
 void part_1(const int *input, const size_t input_size)
 {
-
 	for (size_t a = 0; a < input_size; ++a)
 	{
 		const int candidate_a = input[a];
@@ -39,8 +38,32 @@ void part_1(const int *input, const size_t input_size)
 
 			if (candidate_a + candidate_b == 2020)
 			{
-				printf("Part 1: %d * %d = %d\n", candidate_a, candidate_b, candidate_a * candidate_b);
+				int solution = candidate_a * candidate_b;
+				printf("Part 1: %d * %d = %d\n", candidate_a, candidate_b, solution);
 				return;
+			}
+		}
+	}
+}
+
+void part_2(const int *input, const size_t input_size)
+{
+	for (size_t a = 0; a < input_size; ++a)
+	{
+		const int candidate_a = input[a];
+		for (size_t b = 0; b < input_size; ++b)
+		{
+			const int candidate_b = input[b];
+			for (size_t c = 0; c < input_size; ++c)
+			{
+				const int candidate_c = input[c];
+
+				if (candidate_a + candidate_b + candidate_c == 2020)
+				{
+					int solution = candidate_a * candidate_b * candidate_c;
+					printf("Part 2: %d * %d * %d = %d\n", candidate_a, candidate_b, candidate_c, solution);
+					return;
+				}
 			}
 		}
 	}
@@ -50,7 +73,9 @@ int main(int argc, char *argv[])
 {
 	const size_t input_size = 200;
 	int *input = read_input(input_size);
+
 	part_1(input, input_size);
+	part_2(input, input_size);
 
 	free(input);
 	return 0;
