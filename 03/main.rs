184de794ed91f92fb4861054cf63298c24ee5726
Niklas Mollenhauer <nikeee@outlook.com>
2020-12-04 10:03:34 +0100

    Solve 3: 1/2

---
+++ 03/main.rs
@@ -0,0 +1,66 @@
+// Compile:
+//     rustc main.rs
+// Use:
+//     ./main < input.txt
+
+use std::io::{self, BufRead};
+use std::vec::Vec;
+
+fn main() {
+	let (map, map_width) = read_input();
+	part_1(map, map_width)
+}
+
+fn read_input() -> (Vec<Vec<bool>>, usize) {
+	let mut rows = Vec::new();
+
+	let mut first_row_width: Option<usize> = None;
+
+	let stdin = io::stdin();
+	for wrapped_line in stdin.lock().lines() {
+		let line = wrapped_line.unwrap();
+
+		if first_row_width == None {
+			first_row_width = Some(line.len());
+		} else if first_row_width != Some(line.len()) {
+			panic!("Invalid input");
+		}
+
+		let mut columns = Vec::new();
+
+		for item in line.chars() {
+			columns.push(item == '#')
+		}
+
+		rows.push(columns);
+	}
+
+	(rows, first_row_width.unwrap())
+}
+
+fn part_1(map: Vec<Vec<bool>>, map_width: usize) {
+	let tree_count = traverse_map(map, map_width, 3, 1);
+	println!("Number of trees when traversing (3, 1); Part 1: {}", tree_count)
+}
+
+fn traverse_map(map: Vec<Vec<bool>>, map_width: usize, dir_x: usize, dir_y: usize) -> i32 {
+
+	let (mut pos_x, mut pos_y) = (0, 0);
+	let target_y = map.len();
+	let mut tree_count = 0;
+
+	while pos_y < target_y {
+
+		let wrapped_x = pos_x % map_width;
+
+		let has_tree = map[pos_y][wrapped_x];
+		if has_tree {
+			tree_count += 1;
+		}
+
+		pos_x += dir_x;
+		pos_y += dir_y;
+	}
+
+	tree_count
+}
