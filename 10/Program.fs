ec82ef62f0ca7f27405a5dd2fe5b7d99972a6d15
Niklas Mollenhauer <nikeee@outlook.com>
2020-12-10 21:00:24 +0100

    Solve 10: 2/2

--- 10/Program.fs
+++ 10/Program.fs
@@ -1,15 +1,5 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
-
-type Adapter = {
-    joltage: int
-    next: Adapter list
-}
+open System.Collections.Generic
 
 let rec readlines () = seq {
     let line = Console.ReadLine()
@@ -21,32 +11,30 @@ let rec readlines () = seq {
 let adapterFits source target =
     source < target && source >= target - 3
 
-let fittingAdapters unlinkedAdapters source =
-    unlinkedAdapters
-        |> List.filter (adapterFits source)
+let fittingAdapters allAdapters source =
+    allAdapters
+        |> Set.filter (adapterFits source)
 
-type DeltaCount = {
-    oneDeltas: int
-    threeDeltas: int
+type DeltaStats = {
+    OneDeltas: int
+    ThreeDeltas: int
 }
 
+type Path = int list
 
 [<EntryPoint>]
 let main argv =
     let lines = readlines()
 
-    let unlinkedAdapters =
+    let input =
         lines
         |> Seq.map Int32.Parse
-        |> Seq.toList
+        |> Set.ofSeq
 
-    // unlinkedAdapters |> List.iter (printfn "%d")
-
-
-    let lastAdapter = (List.reduce max unlinkedAdapters) + 3
+    let lastAdapter = input.MaximumElement + 3
     printfn "Laster Adapter: %d" lastAdapter
 
-    let allAdapters = unlinkedAdapters @ [lastAdapter]
+    let allAdapters = Set.add 0 (Set.add lastAdapter input)
     let getFittingAdapters = fittingAdapters allAdapters
 
     let rec traverseAdapters current currentDeltas =
@@ -54,21 +42,53 @@ let main argv =
         then currentDeltas
         else (
                 let fittingAdapters = getFittingAdapters current
-                match fittingAdapters with
-                | [] -> currentDeltas
-                | _ -> (
-                        let minAdapter = List.reduce min fittingAdapters
+                if fittingAdapters.IsEmpty then currentDeltas
+                else (
+                        let minAdapter = fittingAdapters.MinimumElement
                         let nextDeltas = traverseAdapters minAdapter currentDeltas
 
                         let delta = minAdapter - current
                         match delta with
-                        | 1 -> {nextDeltas with oneDeltas = nextDeltas.oneDeltas + 1}
-                        | 3 -> {nextDeltas with threeDeltas = nextDeltas.threeDeltas + 1}
+                        | 1 -> {nextDeltas with OneDeltas = nextDeltas.OneDeltas + 1}
+                        | 3 -> {nextDeltas with ThreeDeltas = nextDeltas.ThreeDeltas + 1}
                         | _ -> nextDeltas
                 )
         )
 
-    let a = traverseAdapters 0 {oneDeltas=0; threeDeltas=0}
-    printfn "Part 1: %d (1-joints: %d, 3-joints: %d)" (a.oneDeltas * a.threeDeltas) a.oneDeltas a.threeDeltas
+    let part1 = traverseAdapters 0 {OneDeltas=0; ThreeDeltas=0}
+    printfn "Always combining the lowest adapter; Part 1: %d (1-joints: %d, 3-joints: %d)" (part1.OneDeltas * part1.ThreeDeltas) part1.OneDeltas part1.ThreeDeltas
+
+    // Part 2
+
+    (*
+    The Problem can be interpreted in a directed graph.
+    An adapter is a node. It has an outgoing connection to another adapter iff it outputs a joltage that the other adapter can receive.
+    We add two artificial nodes: 0 (for the start) and max(joltage) + 3 as end.
+    The task is to find the number ofhamiltonian paths that starts at node 0 and ends at node max(joltage) + 3.
+
+    Notes:
+    - Every path constructed is unique
+    - The graph is acyclic
+    - We have a defined start and end
+    - We only care about the number of paths, not the paths itself
+    *)
+
+    let totalGraphData = Set.add 0 allAdapters
+
+    let getPrevNodes target =
+        allAdapters |> Seq.filter (fun c -> adapterFits c target)
+
+    let rec countHamiltonPaths (currentNode: int, paths: Dictionary<int, int64>): int64 =
+        match currentNode with
+        | 0 -> 1L
+        | _ -> getPrevNodes currentNode |> Seq.sumBy (fun n -> paths.[n])
+
+    let visitedNodes = new Dictionary<int, int64>()
+    for node in (totalGraphData |> Seq.sort) do
+        let nodeValue = countHamiltonPaths (node, visitedNodes)
+        visitedNodes.[node] <- nodeValue
+
+    let part2 = countHamiltonPaths (totalGraphData.MaximumElement, visitedNodes)
+    printfn "Possible number of was combining the input; Part 2: %d" part2
 
     0
