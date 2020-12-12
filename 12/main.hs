f83810102b0eba7fd2f31766cb4b6cc1fee33c99
Niklas Mollenhauer <nikeee@outlook.com>
2020-12-12 20:47:03 +0100

    Solve 12: 2/2

--- 12/main.hs
+++ 12/main.hs
@@ -9,6 +9,13 @@ data Part1State = Part1State {
     orientation :: Integer
 }
 
+data Part2State = Part2State {
+    x :: Integer,
+    y :: Integer,
+    waypointX :: Integer,
+    waypointY :: Integer
+}
+
 data Instruction = Char Integer
 
 parseInstruction instruction = do
@@ -36,12 +43,46 @@ reducePart1 state (operation, offset) = do
             180 -> state { shipX = x - offset }
             270 -> state { shipY = y + offset }
 
+reducePart2 :: Part2State -> (Char, Integer) -> Part2State
+reducePart2 state (operation, offset) = do
+    let wX = waypointX state
+    let wY = waypointY state
+
+    case operation of
+        'N' -> state { waypointY = wY - offset }
+        'S' -> state { waypointY = wY + offset }
+        'E' -> state { waypointX = wX + offset }
+        'W' -> state { waypointX = wX - offset }
+        'L' -> rotate state (360 - (offset `mod` 360))
+        'R' -> rotate state (offset `mod` 360)
+        'F' -> state {
+            x = (x state) + wX * offset,
+            y = (y state) + wY * offset
+        }
+
+rotate state degree = do
+    let wX = waypointX state
+    let wY = waypointY state
+
+    case degree of
+        0 -> state
+        90 -> state { waypointX = -wY, waypointY = wX }
+        180 -> state { waypointX = -wX, waypointY = -wY }
+        270 -> state { waypointX = wY, waypointY = -wX }
+
+manhattanDistance x y = (abs x) + (abs y)
+
 main = do
     input <- getContents
     let instructions = map parseInstruction (lines input)
 
-    let initialState = Part1State 0 0 0
+    let initialState1 = Part1State 0 0 0
+    let part1State = foldl reducePart1 initialState1 instructions
+    let part1 = manhattanDistance (shipX part1State) (shipY part1State)
+    print part1
 
-    let part1 = foldl reducePart1 initialState instructions
-    print ((abs (shipX part1)) + (abs (shipY part1)))
+    let initialState2 = Part2State 0 0 10 (-1)
+    let part2State = foldl reducePart2 initialState2 instructions
+    let part2 = manhattanDistance (x part2State) (y part2State)
+    print part2
 
