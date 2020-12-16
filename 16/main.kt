175805d0973661779b1bdc1a93a0e14a99db4b3e
Niklas Mollenhauer <nikeee@outlook.com>
2020-12-16 18:28:34 +0100

    Solve 16: 2/2

--- 16/main.kt
+++ 16/main.kt
@@ -50,24 +50,69 @@ fun main(args: Array<String>) {
         line = readLine()!!
     }
 
-    println(ticketFields)
-    println(myTicket)
-    println(nearbyTickets)
-
     val solution1 = part1(ticketFields, nearbyTickets)
     println("Sum of error rates in nearby tickets; Part 1: $solution1")
+
+    val solution2 = part2(ticketFields, nearbyTickets, myTicket)
+    println("Multiplication of departure fields in my ticket; Part 2: $solution2")
 }
 
 fun part1(fields: List<TicketField>, nearbyTickets: List<List<Int>>): Int {
-    return nearbyTickets.map{getErrorRate(fields, it)}.sum()
+    return nearbyTickets.map { validateTicket(fields, it) }.map { it.second }.sum()
 }
 
-fun getErrorRate(fields: List<TicketField>, ticket: List<Int>): Int {
+fun validateTicket(fields: List<TicketField>, ticket: List<Int>): Pair<Boolean, Int> {
     val errors = ticket.filter { !isValidNumber(fields, it) }.toList()
     assert(errors.size in 0..1)
-    return errors.sum()
+
+    // There is a zero in the ticket data. It will not contribute to the sum, but should be counted as an error
+    return Pair(errors.isEmpty(), errors.sum())
 }
 
 fun isValidNumber(fields: List<TicketField>, number: Int): Boolean {
-    return fields.any {it.contains(number)}
+    return fields.any { it.contains(number) }
+}
+
+fun part2(fields: List<TicketField>, nearbyTickets: List<List<Int>>, myTicket: List<Int>): Long {
+
+    val validTickets = nearbyTickets
+        .filter { validateTicket(fields, it).first }
+        .toSet()
+
+    val possibleFieldsForColumns = fields.indices.associateBy({ it }, { columnIndex ->
+        val numbersOfColumn = validTickets.map { it[columnIndex] }.toSet()
+
+        fields
+            .filter { field -> numbersOfColumn.all { field.contains(it) } }
+            .toSet()
+
+    }).toMutableMap()
+
+    while (possibleFieldsForColumns.any { it.value.size > 1 }) {
+        val fieldsToRemove = possibleFieldsForColumns.values
+            .filter { it.size == 1 }
+            .reduce { a, b -> a union b }
+            .toSet()
+
+        val affectedKeys = possibleFieldsForColumns
+            .filter { it.value.size > 1 }
+            .map { it.key }
+            .toSet()
+
+
+        for (key in affectedKeys) {
+            val column = possibleFieldsForColumns[key]
+            assert(column != null);
+
+            val newColumnData = column!!.toMutableSet()
+            newColumnData.removeAll(fieldsToRemove)
+            possibleFieldsForColumns[key] = newColumnData
+        }
+    }
+
+    return possibleFieldsForColumns
+        .mapValues { it.value.first() }
+        .filter { it.value.name.startsWith("departure") }
+        .map { myTicket[it.key].toLong() }
+        .reduce { a, b -> a * b }
 }
